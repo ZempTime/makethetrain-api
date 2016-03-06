f1559b54181a8777fd85ad84a06e825785383a8d
Chris Zempel <zempel@efeqdev.com>
2016-03-06 15:43:07 -0600

    depth first search

---
+++ app/models/depth_first_search.rb
@@ -0,0 +1,29 @@
+class DepthFirstSearch
+  attr_accessor :graph, :edges
+
+  def initialize(graph, edges)
+    @graph = Array.new(graph)
+    @edges = Array.new(edges)
+  end
+
+  def search
+    @time = 0
+    @graph.each do |v|
+      visit(graph, v) if v.color == "WHITE"
+    end
+    @graph
+  end
+
+  def visit(graph, u)
+    @time += 1
+    u.d = @time
+    u.color = "GRAY"
+    adjacent_vertices = @edges.select { |e| e.include?(u)}
+    adjacent_vertices.each do |v|
+      visit(graph, v) if v.color == "WHITE"
+    end
+    u.color == "BLACK"
+    @time += 1
+    u.f = @time
+  end
+end
--- app/models/path.rb
+++ app/models/path.rb
@@ -1,20 +1,90 @@
 class Path
+  attr_accessor :stops, :edges, :graph
   # based on: https://en.wikipedia.org/wiki/List_of_MetroLink_(St._Louis)_stations#/media/File:MetroLink_map_Oct2008.svg
+
+  # trips contain a number of stops at specific times
+  # given start/stop, need to determine the # of trips to find, and what stops
+  # determine shortest distance, # of stops, need to consider transfers at different times?
+  #   -> make query for forthcoming stops based on
 
   EAST = "0"
   WEST = "1"
 
-  BLUE_LINE = [
-    "SHREWSBURY", "SUNNEN", "MAPLEWOOD", "BRENTWOOD", "RICHMOND HEIGHTS", "CLAYTON", "FORSYTH", "U CITY BIG BEND", "SKINKER",
-    "FOREST PARK METROLINK STATION", "CENTRAL WEST END", "GRAND", "UNION STA", "CIVIC CENTER", "STADIUM", "8TH AND PINE", "CONVENTION CENTER", "LACLEDES LANDING", "EAST RIVERFRONT", "5TH & MISSOURI", "EMERSON PARK", "JJK CENTER", "WASHINGTON PARK", "FAIRVIEW HEIGHTS"
-  ]
+  def self.depth_first_search
+    path = self.new
+    dfs = DepthFirstSearch.new(path.graph, path.edges)
+    graph = dfs.search
+  end
 
-  RED_LINE = [
-    "LAMBERT MAIN TRML", "LAMBERT EAST TRML", "NORTH HANLEY", "UMSL NORTH", "UMSL SOUTH", "ROCK ROAD", "WELLSTON", "DELMAR",
-    "FOREST PARK", "CENTRAL WEST END", "GRAND", "UNION STA", "CIVIC CENTER", "STADIUM", "8TH AND PINE", "CONVENTION CENTER", "LACLEDES LANDING", "EAST RIVERFRONT", "5TH & MISSOURI", "EMERSON PARK", "JJK CENTER", "WASHINGTON PARK", "FAIRVIEW HEIGHTS",
-    "MEMORIAL HOSPITAL", "SWANSEA", "BELLEVILLE", "COLLEGE", "SHILOH-SCOTT"
-  ]
+  def initialize
+    @stops = Stop.all.to_a
+    @edges = set_edges
+    @graph = @stops.map {|stop| Vertice.new(stop)}
+  end
 
-  def initialize(from, to)
+  def edge_to_stations(edge)
+    [@stops[edge[0]], @stops[edge[1]]]
+  end
+
+  def set_edges
+    result = []
+    result += array_to_edges(stop_names_to_ids(blue_line_names))
+    result += array_to_edges(stop_names_to_ids(red_line_names))
+    result
+  end
+
+  def array_to_edges(array)
+    result = []
+    array.each_index do |i|
+      next if i == 0
+      result.push([array[i - 1], array[i]])
+    end
+    result
+  end
+
+  def blue_line_names
+    [
+      "SHREWSBURY", "SUNNEN", "MAPLEWOOD", "BRENTWOOD", "RICHMOND HEIGHTS", "CLAYTON", "FORSYTH", "U CITY BIG BEND", "SKINKER",
+      "FOREST PARK", "CENTRAL WEST END", "GRAND", "UNION STA", "CIVIC CENTER", "STADIUM", "8TH AND PINE", "CONVENTION CENTER", "LACLEDES LANDING", "EAST RIVERFRONT", "5TH & MISSOURI", "EMERSON PARK", "JJK CENTER", "WASHINGTON PARK", "FAIRVIEW HEIGHTS"
+    ]
+  end
+
+  def stop_names_to_ids(array)
+    array.map do |name|
+      stop = Stop.where(stop_name: "#{name} METROLINK STATION").first
+      @stops.index(stop)
+    end
+  end
+
+  def red_line_names
+    [
+      "LAMBERT MAIN TRML", "LAMBERT EAST TRML", "NORTH HANLEY", "UMSL NORTH", "UMSL SOUTH", "ROCK ROAD", "WELLSTON", "DELMAR",
+      "FOREST PARK", "CENTRAL WEST END", "GRAND", "UNION STA", "CIVIC CENTER", "STADIUM", "8TH AND PINE", "CONVENTION CENTER", "LACLEDES LANDING", "EAST RIVERFRONT", "5TH & MISSOURI", "EMERSON PARK", "JJK CENTER", "WASHINGTON PARK", "FAIRVIEW HEIGHTS",
+      "MEMORIAL HOSPITAL", "SWANSEA", "BELLEVILLE", "COLLEGE", "SHILOH-SCOTT"
+    ]
+  end
+
+  def red_line
+    red_line_names.map { |s| Stop.where(stop_name: "#{s} METROLINK STATION").first.id}
+  end
+
+
+  class Vertice
+    attr_accessor :p, :color, :d, :f, :stop
+
+    # v.d = when vertex discovered
+    # v.f = when all adjancent vertexes explored
+    # v.p = predecessor
+    # v.color = WHITE (nonexplored), GREY (some explored, some not), BLACK (explored)
+
+    def initialize(stop)
+      @stop = stop
+      @color = "WHITE"
+      @p = nil
+    end
+
+    def predecessors
+      @p
+    end
   end
 end
